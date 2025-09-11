db9d44d35ff2d5fa66f2e024a043c1ba221f9e39
김남규 <NK@NK.com>
2025-09-11 11:19:53 +0900

    break 문 추가

--- 10_break_pass.py
+++ 10_break_pass.py
@@ -1,11 +1,39 @@
 cup = 0
-                        # 또는 running = True
-while True:             # 또는  while running
-    cup += 1
-    print(cup)
-    if cup == 10:
-        break           # 또는 running = False
+                        # 또는 running = True, while running:
+# while True:
+#     cup += 1
+#     print(cup)
+#     if cup == 10:
+#         break           # 또는 running = False
 
-print('while 문 종료')
+# print('while 문 종료')
 
+for i in range(1,10):
+    if i == 3:
+        continue
+    elif i ==6:
+        continue
+    elif i ==9:
+        continue
+    print(i)
 
+print()
+
+for i in range(1,10):
+    if i == 3 or i == 6 or (i == 9):
+        continue
+    print(i)
+
+print()
+
+for i in range(1,10):
+    if i in (3,6,9):
+        continue
+    print(i)
+
+print()
+
+for i in range(1,10):
+    if i % 3 ==0:
+        continue
+    print(i)
