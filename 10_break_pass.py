c085aa981f11b5209ca5b8109d8da4b9cb616805
김남규 <NK@NK.com>
2025-09-11 10:57:09 +0900

    break 문 추가

---
+++ 10_break_pass.py
@@ -0,0 +1,11 @@
+cup = 0
+                        # 또는 running = True
+while True:             # 또는  while running
+    cup += 1
+    print(cup)
+    if cup == 10:
+        break           # 또는 running = False
+
+print('while 문 종료')
+
+
