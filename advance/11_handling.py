a17e78883c900830d750a072505f497894638e73
ngkim <ngkim@ng.kim>
2025-09-16 15:48:00 +0900

    예외 처리 update

--- advance/11_handling.py
+++ advance/11_handling.py
@@ -1,3 +1,5 @@
+import traceback
+
 num_list = [1,2,3,1,2,3,1,2,3,1,2,3,4,5,6]
 
 idx = 0
@@ -6,7 +8,9 @@ try:
         idx = num_list.index(3, idx)
         print(f'3은 {idx} 인덱스에 있습니다.')
         idx += 1
-except ValueError:
+except ValueError as e:
+    print(e) # 예외에 대한 대략적인 정보 출력
+    traceback.print_exc() # 상세한 예외 정보를 보여준다.
     print("더 이상 3을 찾을 수 없습니다.")
 finally:
     print("--------끝--------")
