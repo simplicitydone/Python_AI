55958417dae9bfdcfe2b2d3ea642b4d16446fc21
Your New Username <nk@nk.com>
2025-09-16 11:54:07 +0900

    main

---
+++ main.py
@@ -0,0 +1,8 @@
+from com.py.oop.other_module import other_function
+other_function()
+
+from com.py.oop import other_module
+other_module.other_function()
+
+from com.py.oop import other_module as mo
+mo.other_function()
