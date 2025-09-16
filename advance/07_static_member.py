343dc683bd33465eb08abf86cca3cdfe1c3560fa
Your New Username <NK@NK.com>
2025-09-16 11:19:23 +0900

    정적 멤버 정리

--- advance/07_static_member.py
+++ advance/07_static_member.py
@@ -3,9 +3,13 @@ class Robot:
 
     def how_count(self):
         print(f'객체 메서드 : {self.count}')
+
+    # 2. self는 내가 소속된 객체를 의미하는데 원본에서 왔으므로 객체가 없다.
+    # 3. @classmethod 어노테이션을 이용해 원본에서 직접 왔으므로 self는 객체가 아닌 클래스라고 알려줌
+    # 4. 그러니 self라는 이름은 객체를 받는 이자값으로 오해할 수 있으니 cls로 바꾸라고 권고(강제 아님)
     @classmethod #원본영역의 변수를 건드릴 수 있다는 표시
-    def std_count(self):
-        print(f'클래스 메서드 : {self.count}')
+    def std_count(cls):
+        print(f'클래스 메서드 : {cls.count}')
 
 r1 = Robot()
 r2 = Robot()
@@ -27,4 +31,9 @@ r2.how_count()
 
 # 원본의 내용을 확인하고 싶다면 원본영역으로 가서 확인해야 한다.
 print(f'원본 count : {Robot.count}')
+
+# 1. 원본영역에서 함수를 실행하니 self가 없다고 에러가 남
 print(f'원본 함수 : {Robot.std_count()}')
+
+
+
