9d7e6305d55c4274ef07077705e6d9c1805862e1
Your New Username <NK@NK.com>
2025-09-16 11:12:15 +0900

    정적 멤버2

--- advance/07_static_member.py
+++ advance/07_static_member.py
@@ -3,7 +3,28 @@ class Robot:
 
     def how_count(self):
         print(f'객체 메서드 : {self.count}')
-
+    @classmethod #원본영역의 변수를 건드릴 수 있다는 표시
     def std_count(self):
         print(f'클래스 메서드 : {self.count}')
 
+r1 = Robot()
+r2 = Robot()
+# r1과 r2는 서로 다른 객체이므로 count를 건드렸을때 서로 영향받지 않는다.
+r1.count += 1
+print(f'r1.count : {r1.count}')
+print(f'r2.count : {r2.count}')
+r2.count = 100
+print(f'r1.count : {r1.count}')
+print(f'r2.count : {r2.count}')
+
+
+# 원본의 내용을 고치고 싶다면? 원본으로 직접 가서 고쳐야 한다.
+Robot.count = 1000
+print(f'r1.count : {r1.count}')
+print(f'r2.count : {r2.count}')
+r1.how_count()
+r2.how_count()
+
+# 원본의 내용을 확인하고 싶다면 원본영역으로 가서 확인해야 한다.
+print(f'원본 count : {Robot.count}')
+print(f'원본 함수 : {Robot.std_count()}')
